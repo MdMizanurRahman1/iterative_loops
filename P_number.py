0449b05ccca44d524e55b50fec80c353c2e242c7
mizan <mizan389rahman@gmail.com>
2026-02-01 01:37:06 +0200

    number 4 solved

---
+++ P_number.py
@@ -0,0 +1,15 @@
+#4
+
+cities = []
+
+
+for i in range(5):
+    city = input("Please enter the name of a city: ")
+    cities.append(city)
+
+
+#city nam one by one
+for single_city in cities:
+    print(single_city)
+
+
